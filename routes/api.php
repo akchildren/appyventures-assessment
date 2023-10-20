82d973df7b508a2572bab513039bab0a37ae2a39
Adam Children <adam.children@themix.org.uk>
2023-10-20 10:42:37 +0100

    APPY-7: move auth route to api web routes file

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::apiResource('tasks', TaskController::class);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+require __DIR__.'/auth.php';
