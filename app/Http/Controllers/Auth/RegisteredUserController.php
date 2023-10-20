ca44a99bffeb50e07087339c4a7feac7747545ad
Adam Children <adam.children@themix.org.uk>
2023-10-20 10:47:31 +0100

    APPY-7: tidy register user controller packages

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,20 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
 
 class RegisteredUserController extends Controller
 {
     /**
      * Handle an incoming registration request.
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
-    public function store(RegisterRequest $request): Response
+    public function __invoke(RegisterRequest $request): Response
     {
         $user = User::create([
             'name' => $request->name,
