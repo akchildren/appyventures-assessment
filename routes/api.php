00292631573584841e7f6267056e6dfd13542cb9
Adam Children <adam.children@themix.org.uk>
2023-10-20 10:50:40 +0100

    APPY-7: make tasks go behind sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('tasks', TaskController::class);
+Route::apiResource('tasks', TaskController::class)->middleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
