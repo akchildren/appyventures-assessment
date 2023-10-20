938d737520f9d9a295fdd3725bd870a74cbedd1d
Adam Children <adam.children@themix.org.uk>
2023-10-20 11:55:55 +0100

    fix: invokable authentication routes

--- routes/auth.php
+++ routes/auth.php
@@ -9,17 +9,17 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class])
+Route::post('/register', RegisteredUserController::class)
                 ->middleware('guest')
                 ->name('register');
 
-Route::post('/login', [LoginController::class])
+Route::post('/login', LoginController::class)
                 ->middleware('guest')
                 ->name('login');
 
-//Route::post('/logout', [LogoutController::class])
-//    ->middleware('auth')
-//    ->name('logout');
+Route::post('/logout', LogoutController::class)
+    ->middleware('auth')
+    ->name('logout');
 
 //Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
 //                ->middleware('guest')
