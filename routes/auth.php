fb0826895ec218faf30c3686d4d84377359ca7ff
Adam Children <adam.children@themix.org.uk>
2023-10-23 20:39:22 +0100

    Clear unused breeze auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisterUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterUserController::class)
